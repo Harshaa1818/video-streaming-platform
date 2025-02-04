3c838fdd4a4c33539fc2b26bcfcf828920d1db8d
Prashant Patil <patilanilprashant178@gmail.com>
2025-02-04 18:14:56 +0530

    added Prisma Schema and Written SignUp Logic

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Test");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth", authRoutes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
